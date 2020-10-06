35c35548f3f36b509456bf6fec3b7d84313b5741
Bernat <bernat.orellana@gmail.com>
2020-10-06 07:01:43 +0200

    Petit canvi

--- src/sampleproject/SampleProject.java
+++ src/sampleproject/SampleProject.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sampleproject;
 
 /**
@@ -15,7 +10,7 @@ public class SampleProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(">Hello World !");
+        System.out.println(">Hello World 2 !");
     }
     
 }
