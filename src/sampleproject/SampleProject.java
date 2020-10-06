cc533bbf8cb8fb1db4ac91e52b5c896ce28e0fb2
Bernat <bernat.orellana@gmail.com>
2020-10-06 07:15:06 +0200

    Canvi B

--- src/sampleproject/SampleProject.java
+++ src/sampleproject/SampleProject.java
@@ -11,6 +11,7 @@ public class SampleProject {
      */
     public static void main(String[] args) {
         System.out.println(">Hello World 2 !");
+        System.out.println(">Nova línia B");
     }
     
 }
