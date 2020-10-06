4f3a48e00b2eec67746aee13680d0d50a0c4cd18
Bernat <bernat.orellana@gmail.com>
2020-10-06 07:14:54 +0200

    canvi A

--- src/sampleproject/SampleProject.java
+++ src/sampleproject/SampleProject.java
@@ -10,7 +10,7 @@ public class SampleProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(">Hello World 2 !");
+        System.out.println(">Hello World 2 A!");
     }
     
 }
